5c740c95051fecc354be5882c0914a23e2cfba62
alireza khorammak <alirezakhorammak@gmail.com>
2024-07-03 06:52:42 -0700

    Ending Commit Anything

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using DevCard.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController()
+        public HomeController(ILogger<HomeController> logger)
         {
-
+            _logger = logger;
         }
 
         public IActionResult Index()
